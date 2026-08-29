a5a493eedb2c6396364db34275604faa495126ce
Lodya_zw74@top-academy.ru <Lodya_zw74@top-academy.ru>
2026-08-29 17:49:27 +0500

    Fibonacci.razor.cs DONE

---
+++ Blazor/Blazor/Components/Pages/Fibonacci.razors.cs
@@ -0,0 +1,23 @@
+﻿using System.Numerics;
+
+namespace Blazor.Components.Pages
+{
+	public partial class Fibonacci
+	{
+		int n = 0;
+		BigInteger[] fib = { };
+		int N
+		{
+			get => n;
+			set => n = value < 0 ? 0 : value;
+		}
+		void OutputFibonacci()
+		{
+			fib = new BigInteger[n];
+			for (int i = 0; i < n; i++)
+			{
+				fib[i] = i == 0 ? 1 : i == 1 ? 1 : fib[i - 1] + fib[i - 2];
+			}
+		}
+	}
+}
