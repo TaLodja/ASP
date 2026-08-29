c9994cf4766e4a0e8ede5312737629fec92f8a97
Lodya_zw74@top-academy.ru <Lodya_zw74@top-academy.ru>
2026-08-29 16:45:04 +0500

    Power DONE

---
+++ Blazor/Blazor/Components/Pages/Power.razor.cs
@@ -0,0 +1,19 @@
+﻿namespace Blazor.Components.Pages
+{
+	public partial class Power
+	{
+		double a = 1;
+		double n = 0;
+		double result = 0;
+		double A
+		{
+			get => a;
+			set => a = value == 0 ? 1 : value;
+		}
+		void Calculate()
+		{
+			result = 0;
+			result = Math.Pow(a, n);
+		}
+	}
+}
