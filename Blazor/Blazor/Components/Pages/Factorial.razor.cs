922e54905a8c942287453782b92aef1a5baa368f
Lodya_zw74@top-academy.ru <Lodya_zw74@top-academy.ru>
2026-08-29 15:10:00 +0500

    Factorial DONE

---
+++ Blazor/Blazor/Components/Pages/Factorial.razor.cs
@@ -0,0 +1,24 @@
+﻿using System.Numerics;
+
+namespace Blazor.Components.Pages
+{
+	public partial class Factorial		//partial - класс размещен в нескольких файлах
+	{
+		int n = 0;  //RAII - Resource Aquisition is Initialization
+					//		(Выделение ресурсов - это Инициализация)
+		BigInteger f = 1;
+		int N
+		{
+			get => n;
+			set => n = value < 0 ? 0 : value;
+		}
+		void Calculate()
+		{
+			f = 1;
+			for (int i = 1; i <= n; i++)
+			{
+				f *= i;
+			}
+		}
+	}
+}
