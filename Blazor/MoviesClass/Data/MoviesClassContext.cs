e57d2447039201a44e3613ddf41363fa674d46b9
Lodya_zw74@top-academy.ru <Lodya_zw74@top-academy.ru>
2026-09-19 00:58:50 +0500

    Movies CRUD DONE

---
+++ Blazor/MoviesClass/Data/MoviesClassContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using MoviesClass.Models;
+
+namespace MoviesClass.Data
+{
+    public class MoviesClassContext : DbContext
+    {
+        public MoviesClassContext (DbContextOptions<MoviesClassContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<MoviesClass.Models.Movie> Movie { get; set; } = default!;
+    }
+}
