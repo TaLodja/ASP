a9a283f861f560ff4abab853f2181b935806159a
Lodya_zw74@top-academy.ru <Lodya_zw74@top-academy.ru>
2026-09-19 15:45:05 +0500

    Lesson 9 - Directions DONE

--- Blazor/Academy/Models/Direction.cs
+++ Blazor/Academy/Models/Direction.cs
@@ -1,10 +1,12 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Academy.Models
 {
 	public class Direction
 	{
 		[Key]
+		[Column(TypeName = "TINYINT")]
 		public int direction_id { get; set; }
 		public string direction_name { get; set; }
 	}
